2dedb1dd05b70150ea96851261294d1d8318f439
Vladimir <v.v.levikov@gmail.com>
2020-09-04 16:28:21 +0300

    Добавлено движение корабля

--- main.py
+++ main.py
@@ -68,38 +68,38 @@ async def blink(canvas, row, column, symbol='*'):
         for iteration in range(6):
             await asyncio.sleep(0)
 
-async def animate_spaceship(canvas, frame_y, frame_x, frame_1, frame_2):
-    draw_frame(canvas, frame_y, frame_x, frame_1)
+async def animate_spaceship(canvas, frame_row, frame_column, frame_1, frame_2):
+    draw_frame(canvas, frame_row, frame_column, frame_1)
     canvas.refresh()
-    time.sleep(0.1)
     await asyncio.sleep(0)
 
-    draw_frame(canvas, frame_y, frame_x, frame_1, negative=True)
-    draw_frame(canvas, frame_y, frame_x, frame_2)
+    draw_frame(canvas, frame_row, frame_column, frame_1, negative=True)
+    draw_frame(canvas, frame_row, frame_column, frame_2)
     canvas.refresh()
-    time.sleep(0.1)
     await asyncio.sleep(0)
 
-    draw_frame(canvas, frame_y, frame_x, frame_2, negative=True)
-    draw_frame(canvas, frame_y, frame_x, frame_1)
+    draw_frame(canvas, frame_row, frame_column, frame_2, negative=True)
+    draw_frame(canvas, frame_row, frame_column, frame_1)
     canvas.refresh()
-    time.sleep(0.1)
+
+
 
 def draw(canvas):
     canvas.border('|', '|')
     curses.curs_set(False)
+    canvas.nodelay(True)
     max_y, max_x = canvas.getmaxyx()
     coroutines = []
     symbols_for_star = ['+', '*', '.', ':']
-    half_y = int(max_y / 2)
-    half_x =  int (max_x / 2)
-    fire_coroutine = fire(canvas, half_y,half_x)
+    start_rows = int(max_y / 2)
+    start_columns =  int (max_x / 2)
+    fire_coroutine = fire(canvas, start_rows,start_columns)
     with open('./frames/rocket_frame_1.txt', 'r') as frame:
         frame_1 = frame.read()
     with open('./frames/rocket_frame_2.txt', 'r') as frame:
         frame_2 = frame.read()
 
-    spaceship = animate_spaceship(canvas, half_y, half_x, frame_1, frame_2)
+    spaceship = animate_spaceship(canvas, start_rows, start_columns, frame_1, frame_2)
     for star_count in range(1, 200):
         column = random.randint(1, max_x-5)
         row = random.randint(1, max_y-5)
@@ -118,7 +118,12 @@ def draw(canvas):
         try:
             spaceship.send(None)
         except StopIteration:
-            spaceship = animate_spaceship(canvas, half_y, half_x, frame_1, frame_2)
+            row, column, space = read_controls(canvas)
+            if row != 0 or column != 0:
+                draw_frame(canvas, start_rows, start_columns, frame_1, negative=True)
+            start_rows = start_rows + row
+            start_columns = start_columns + column
+            spaceship = animate_spaceship(canvas, start_rows, start_columns, frame_1, frame_2)
 
         try:
             coroutine.send(None)
